ca92de5582bad937ff47a00f2c716c23a5ab9e41
bpx-chain <admin@bpxchain.cc>
2024-01-22 12:34:27 +0100

    BPX Network -> BPX Chain

--- packages/gui/src/components/about/About.tsx
+++ packages/gui/src/components/about/About.tsx
@@ -115,7 +115,7 @@ export default function About(props: Props) {
         </StyledLink>
         <StyledSubTitle>{description}</StyledSubTitle>
         <Spacer />
-        <div className="copyright">Copyright (c) {currentYear} BPX Network</div>
+        <div className="copyright">Copyright (c) {currentYear} BPX Chain</div>
         <Spacer />
         <VersionsTable>
           {versions?.electron && (
